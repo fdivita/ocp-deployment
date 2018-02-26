919dd013a223db8a144f9e666d97cd2e7ae4117d
fabio di vita <fdivita@gmail.com>
2018-02-26 16:33:40 +0100

    deploy metrics

---
+++ deploy_metrics.sh
@@ -0,0 +1,1 @@
+ansible-playbook -f 20 /usr/share/ansible/openshift-ansible/playbooks/byo/openshift-cluster/openshift-metrics.yml -e openshift_metrics_install_metrics=True  -e openshift_metrics_hawkular_hostname=hawkular-metrics.apps.oslab.opentlc.com -e openshift_metrics_cassandra_nodeselector={"region":"infra"}
