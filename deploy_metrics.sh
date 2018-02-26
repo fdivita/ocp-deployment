19fb061842ee911b6a9a13377a26ecd6582866c9
fabio di vita <fdivita@gmail.com>
2018-02-26 20:03:29 +0100

    logging creation

--- deploy_metrics.sh
+++ deploy_metrics.sh
@@ -1,1 +1,11 @@
-ansible-playbook -f 20 /usr/share/ansible/openshift-ansible/playbooks/byo/openshift-cluster/openshift-metrics.yml -e openshift_metrics_install_metrics=True  -e openshift_metrics_hawkular_hostname=hawkular-metrics.apps.oslab.opentlc.com -e openshift_metrics_cassandra_nodeselector={"region":"infra"}
+#######################
+#login as system:admin
+
+
+
+
+
+ansible-playbook -f 20 /usr/share/ansible/openshift-ansible/playbooks/byo/openshift-cluster/openshift-metrics.yml -e openshift_metrics_install_metrics=True  -e openshift_metrics_hawkular_hostname=hawkular-metrics.apps.oslab.opentlc.com 
+-e openshift_metrics_cassandra_nodeselector={"region":"infra"}
+
+ansible-playbook -f 20 /usr/share/ansible/openshift-ansible/playbooks/byo/openshift-cluster/openshift-logging.yml -e openshift_logging_install_logging=True -e openshift_logging_master_public_url=master1-80c1.oslab.opentlc.com
